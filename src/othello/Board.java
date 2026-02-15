0572a777fc8c0e9f0f59e1d555c9ee93cdccaadf
Akhil Raj <akhilrajwork@gmail.com>
2026-02-15 16:24:20 +0530

    feat: initialize Othello board structure with empty grid

--- src/othello/Board.java
+++ src/othello/Board.java
@@ -2,4 +2,25 @@ package othello;
 
 public class Board {
 
+    private final int boardSize = 8;
+
+    private char[][] board;
+
+    private char p1Symbol;
+
+    private int count;
+
+    private static final char EMPTY = ' ';
+
+    public Board(char p1Symbol){
+        board = new char[boardSize][boardSize];
+        for(int i = 0; i < boardSize; i++){
+            for(int j = 0; j < boardSize; j++){
+                board[i][j] = EMPTY;
+            }
+        }
+        //Currently the board assumes a 2 player game with fixed symbols
+        this.p1Symbol = p1Symbol;
+    }
+
 }
