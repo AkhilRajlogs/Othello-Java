ffb9ca1fb9173f99da31983f03fcfbf7c461fd3d
Akhil Raj <akhilrajwork@gmail.com>
2026-02-18 06:10:39 +0530

    feat(game): add player switching logic in Othello

--- src/othello/Othello.java
+++ src/othello/Othello.java
@@ -4,10 +4,20 @@ public class Othello {
 
     private Board board;
     private char currentPlayer;
+    private final char p1Symbol, p2Symbol;
 
     public Othello(char p1Symbol, char p2Symbol) {
+
         this.board = new Board(p1Symbol, p2Symbol);
-        this.currentPlayer = p1Symbol;
+        this.p1Symbol = p1Symbol;
+        this.p2Symbol = p2Symbol;
+
+        currentPlayer = p1Symbol;
+
+    }
+
+    private void switchPlayer() {
+        currentPlayer = (currentPlayer == p1Symbol) ? p2Symbol : p1Symbol;
     }
 
 }
